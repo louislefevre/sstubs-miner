43ab025438f9cc75dab2a018127627ba526bedd0
Louis Lefevre <louislefev@gmail.com>
2020-12-12 00:15:43 +0000

    Fix JSON writing error
    The JSON file was closing before being written. Turns out calling
    close() isn't necessary, as 'with' will automatically close the file
    itself.

--- JsonManager.py
+++ JsonManager.py
@@ -3,7 +3,7 @@ import json
 
 class JsonWriter:
     def __init__(self, file_name):
-        self._file = open(file_name, 'a')
+        self._file_name = file_name
 
     def write_object(self, obj):
         self._write(obj.__dict__)
@@ -12,12 +12,8 @@ class JsonWriter:
         self._write(dictionary)
 
     def _write(self, data):
-        with self._file as json_file:
+        with open(self._file_name, 'a') as json_file:
             json.dump(data, json_file, indent=4)
-        json_file.close()
-
-    def close(self):
-        self._file.close()
 
 
 class JsonReader:
