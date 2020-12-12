6b9681dc94824409088240bd492b802e92876ec1
Louis Lefevre <louislefev@gmail.com>
2020-12-12 02:01:04 +0000

    Rework BuildMiner.py
    - Converted BuildMiner.py functionality into a class.
    - BuildMiner.py output is now directed into a JSON file, instead of
      being printed to the console.
    - Added a 'none' build by default.
    - Fixed build mining status update printing.
    - Removed '.json' extension assumption from JsonManager.py.

--- BuildMiner.py
+++ BuildMiner.py
@@ -1,81 +1,73 @@
-# Generate a dictionary containing the files the program will check for.
 from JsonManager import JsonWriter
 
 
-def load_builds(path):
-    builds = {}
-    with open(path) as file:
-        for line in file:
-            builds[line.strip()] = 0
-    return builds
+class BuildMiner:
+    def __init__(self, github, sstubs, builds_file='data/builds.txt', output_file='results/builds.json'):
+        self._github = github
+        self._sstubs = sstubs
+        self._projects = self._load_projects(sstubs)
+        self._builds = self._load_builds(builds_file)
+        self._output_file = output_file
+        self._counter = 0
 
+    def mine(self):
+        self._mine_builds()
+        self._add_builds()
+        self._write_builds()
 
-def load_names(sstubs):
-    project_names_set = set()
-    for sstub in sstubs:
-        project_names_set.add(sstub.project_name)
+    def _mine_builds(self):
+        for project_name in self._projects.keys():
+            self._update_status(project_name)
+            repo = self._github.get_repo(project_name)
+            contents = repo.get_contents('')
 
-    project_names_dict = {}
-    for name in project_names_set:
-        project_names_dict[name] = ''
-
-    return project_names_dict
-
-
-# Accesses GitHub using a personal access token.
-# Iterates through the projects retrieved from the dataset, and retrieves their top-level contents.
-# For each file in the repositories contents, check it against the build systems dictionary.
-def mine_repos(github, project_names, builds):
-    counter = 0
-    
-    for name in project_names.keys():
-        counter += 1
-        blank_string = '                                   '
-        print('({}/{}) {}{}'.format(counter, len(project_names), name, blank_string), end='\r')
-
-        repo = github.get_repo(name)
-        contents = repo.get_contents('')
-
-        for content_file in contents:
-            file_name = content_file.name.lower()
-
-            for key in builds.keys():
-                if file_name == key:
-                    project_names[name] = key
-                    builds[key] += 1
-                    break
+            for file in contents:
+                file_name = file.name.lower()
+                for build_name in self._builds.keys():
+                    if file_name == build_name:
+                        self._projects[project_name] = build_name
+                        self._builds[build_name] += 1
+                        break
+                else:
+                    continue
+                break
             else:
-                continue
-            break
+                self._builds['none'] += 1
 
+    def _add_builds(self):
+        for sstub in self._sstubs:
+            for name, build in self._projects.items():
+                if sstub.project_name == name:
+                    sstub.build_system = build
 
-def add_builds(sstubs, project_names):
-    for sstub in sstubs:
-        for name, build in project_names.items():
-            if sstub.project_name == name:
-                sstub.build_system = build
+    def _write_builds(self):
+        writer = JsonWriter(self._output_file)
+        writer.write(self._builds)
 
+    def _update_status(self, name):
+        self._counter += 1
+        total_projects = len(self._projects)
+        blank_string = '                                   '
+        print('({}/{}) {}{}'.format(self._counter, total_projects, name, blank_string), end='\r')
+        if self._counter == total_projects:
+            print()
 
-# Print the results for each build system.
-# Also prints the amount of projects which the program could not find a build system for.
-def print_results(builds, project_names):
-    total_projects = len(project_names)
-    total_builds = 0
+    @staticmethod
+    def _load_projects(sstubs):
+        projects_set = set()
+        for sstub in sstubs:
+            projects_set.add(sstub.project_name)
 
-    for key, value in builds.items():
-        total_builds += value
-        print('{} has {} occurrences.'.format(key, value))
-    print('{} projects had another or no build system.'.format(total_projects-total_builds))
+        project_dict = {}
+        for name in projects_set:
+            project_dict[name] = ''
 
+        return project_dict
 
-def mine(github, sstubs):
-    build_names = load_builds('data/builds.txt')
-    project_names = load_names(sstubs)
-
-    mine_repos(github, project_names, build_names)
-    add_builds(sstubs, project_names)
-
-    writer = JsonWriter('results/builds')
-    writer.write(build_names)
-
-    print_results(build_names, project_names)
+    @staticmethod
+    def _load_builds(builds_file):
+        builds = {'none': 0}
+        with open(builds_file) as file:
+            for line in file:
+                builds[line.strip()] = 0
+        return builds
