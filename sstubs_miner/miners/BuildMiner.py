470580109901ede356102fb9a5df801db4085623
Louis Lefevre <louislefev@gmail.com>
2020-12-21 13:02:35 +0000

    Remove build counting
    Removed redundant code previously used for counting builds in
    BuildMiner.py, since this task will now be performed during analysis
    instead.

--- sstubs_miner/miners/BuildMiner.py
+++ sstubs_miner/miners/BuildMiner.py
@@ -2,8 +2,8 @@ class BuildMiner:
     def __init__(self, github):
         self._github = github
         self._projects = {}
-        self._builds = {'build.gradle': 0, 'build.gradle.kts': 0, 'pom.xml': 0,
-                        'build.bazel': 0, 'build': 0, 'none': 0}
+        self._builds = ['build.gradle', 'build.gradle.kts',
+                        'pom.xml', 'build.bazel', 'build']
 
     def mine(self, sstub):
         project_name = sstub.project_name
@@ -14,14 +14,11 @@ class BuildMiner:
         contents = self._github.get_contents(project_name, '')
         for file in contents:
             file_name = file.name.lower()
-            for build_name in self._builds.keys():
+            for build_name in self._builds:
                 if file_name == build_name:
                     sstub.build_system = build_name
                     self._projects[project_name] = build_name
-                    self._builds[build_name] += 1
                     break
             else:
                 continue
             break
-        else:
-            self._builds['none'] += 1
